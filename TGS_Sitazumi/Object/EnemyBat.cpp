fa9d3769dbcc914be66d370124f5aa84df64a485
れお <120348353+yamaguchi-reoo@users.noreply.github.com>
2024-04-15 13:39:36 +0900

    コウモリ描画

--- TGS_Sitazumi/Object/EnemyBat.cpp
+++ TGS_Sitazumi/Object/EnemyBat.cpp
@@ -23,22 +23,21 @@ void EnemyBat::Update(GameMain* main)
 void EnemyBat::Draw() const
 {
 	DrawFormatString(200, 200, GetColor(255, 255, 225), "%d   %d", KeyInput::GetMouseCursor().x, KeyInput::GetMouseCursor().y);
-	//DrawQuadrangle
 	//é®
-	DrawTriangle(850, 300, 850, 325, 862, 312, GetColor(255, 255, 255), TRUE);
-	DrawTriangle(880, 300, 880, 325, 868, 312, GetColor(255, 255, 255), TRUE);
+	DrawTriangle(850, 300, 850, 325, 862, 312, GetColor(255, 255, 255), FALSE);
+	DrawTriangle(880, 300, 880, 325, 868, 312, GetColor(255, 255, 255), FALSE);
 	//ì™
-	DrawQuadrangle(865, 316, 880, 330, 865, 340, 850, 330, GetColor(255, 255, 255), TRUE);
+	DrawQuadrangle(865, 316, 880, 330, 865, 340, 850, 330, GetColor(255, 255, 255), FALSE);
 	//ì∑ëÃ
-	DrawQuadrangle(865, 345, 880, 368, 865, 395, 850, 368, GetColor(255, 255, 255), TRUE);
+	DrawQuadrangle(865, 345, 880, 368, 865, 395, 850, 368, GetColor(255, 255, 255), FALSE);
 	//âEóÉ
-	DrawTriangle(877, 344, 917, 310, 943, 385, GetColor(255, 255, 255), TRUE);
-	DrawTriangle(877, 344, 917, 310, 916, 380, GetColor(255, 255, 255), TRUE);
-	DrawTriangle(875, 344, 917, 310, 895, 375, GetColor(255, 255, 255), TRUE);
+	DrawTriangle(877, 344, 917, 310, 943, 385, GetColor(255, 255, 255), FALSE);
+	DrawTriangle(877, 344, 917, 310, 916, 380, GetColor(255, 255, 255), FALSE);
+	DrawTriangle(875, 344, 917, 310, 895, 375, GetColor(255, 255, 255), FALSE);
 	//ç∂óÉ
-	/*DrawTriangle(853, 344, 917, 310, 943, 385, GetColor(255, 255, 255), TRUE);
-	DrawTriangle(853, 344, 917, 310, 916, 380, GetColor(255, 255, 255), TRUE);
-	DrawTriangle(853, 344, 917, 310, 895, 375, GetColor(255, 255, 255), TRUE);*/
+	DrawTriangle(853, 344, 816, 310, 787, 385, GetColor(255, 255, 255), FALSE);
+	DrawTriangle(853, 344, 816, 310, 814, 380, GetColor(255, 255, 255), FALSE);
+	DrawTriangle(855, 344, 816, 310, 835, 375, GetColor(255, 255, 255), FALSE);
 
 
 }
