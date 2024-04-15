0f16e998edfe92025c838ce906e1d65c459259fa
れお <120348353+yamaguchi-reoo@users.noreply.github.com>
2024-04-15 13:46:08 +0900

    Update EnemyBat.cpp

--- TGS_Sitazumi/Object/EnemyBat.cpp
+++ TGS_Sitazumi/Object/EnemyBat.cpp
@@ -23,21 +23,19 @@ void EnemyBat::Update(GameMain* main)
 void EnemyBat::Draw() const
 {
 	DrawFormatString(200, 200, GetColor(255, 255, 225), "%d   %d", KeyInput::GetMouseCursor().x, KeyInput::GetMouseCursor().y);
-	//é®
+	//ËÄ≥
 	DrawTriangle(850, 300, 850, 325, 862, 312, GetColor(255, 255, 255), FALSE);
 	DrawTriangle(880, 300, 880, 325, 868, 312, GetColor(255, 255, 255), FALSE);
-	//ì™
+	//È†≠
 	DrawQuadrangle(865, 316, 880, 330, 865, 340, 850, 330, GetColor(255, 255, 255), FALSE);
-	//ì∑ëÃ
+	//ËÉ¥‰Ωì
 	DrawQuadrangle(865, 345, 880, 368, 865, 395, 850, 368, GetColor(255, 255, 255), FALSE);
-	//âEóÉ
+	//Âè≥Áøº
 	DrawTriangle(877, 344, 917, 310, 943, 385, GetColor(255, 255, 255), FALSE);
 	DrawTriangle(877, 344, 917, 310, 916, 380, GetColor(255, 255, 255), FALSE);
 	DrawTriangle(875, 344, 917, 310, 895, 375, GetColor(255, 255, 255), FALSE);
-	//ç∂óÉ
+	//Â∑¶Áøº
 	DrawTriangle(853, 344, 816, 310, 787, 385, GetColor(255, 255, 255), FALSE);
 	DrawTriangle(853, 344, 816, 310, 814, 380, GetColor(255, 255, 255), FALSE);
 	DrawTriangle(855, 344, 816, 310, 835, 375, GetColor(255, 255, 255), FALSE);
-
-
 }
