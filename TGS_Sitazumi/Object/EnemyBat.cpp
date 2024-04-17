8e3f098065ad244a08f640858a86721d3035c907
れお <120348353+yamaguchi-reoo@users.noreply.github.com>
2024-04-17 11:42:23 +0900

    コウモリにワールド座標を与える（1%）

--- TGS_Sitazumi/Object/EnemyBat.cpp
+++ TGS_Sitazumi/Object/EnemyBat.cpp
@@ -4,23 +4,6 @@
 EnemyBat::EnemyBat()
 {
 	type = ENEMY;
-	x_move = 0;
-	local_location.x = 877;
-	local_location.y = 300;
-	erea.width = 120;
-	erea.height = 100;
-	//vertices = {
-	//	{850,300,850,325,862,312},			//耳
-	//	{880,300,880,325,868,312},			//耳
-	//	{865,316,880,330,865,340,850,330},  //頭
-	//	{865,345,880,368,865,395,850,330},	//胴体
-	//	{877,344,917,310,943,385},			//右翼
-	//	{877,344,917,310,916,380},
-	//	{875,344,917,310,895,375},
-	//	{853,334,816,310,787,385},			//左翼
-	//	{853,334,816,310,814,380},
-	//	{855,334,816,310,835,375},
-	//};
 }
 
 EnemyBat::~EnemyBat()
@@ -29,58 +12,39 @@ EnemyBat::~EnemyBat()
 
 void EnemyBat::Update()
 {
-	if (++x_move > 60) {
-		x_move = 0;
-	}
 	Move();
 	BatArray();
 }
 
 void EnemyBat::Draw() const
 {
-	//耳
-	DrawTriangle(850, 300, 850, 325, 862, 312, GetColor(255, 255, 255), TRUE);
-	DrawTriangle(880, 300, 880, 325, 868, 312, GetColor(255, 255, 255), TRUE);
-	//頭
-	DrawQuadrangle(865, 316, 880, 330, 865, 340, 850, 330, GetColor(255, 255, 255), TRUE);
-	//胴体
-	DrawQuadrangle(865, 345, 880, 368, 865, 395, 850, 368, GetColor(255, 255, 255), TRUE);
-	//右翼
-	DrawTriangle(877, 344, 917, 310, 943, 385, GetColor(255, 255, 255), TRUE);
-	DrawTriangle(877, 344, 917, 310, 916, 380, GetColor(255, 255, 255), TRUE);
-	DrawTriangle(875, 344, 917, 310, 895, 375, GetColor(255, 255, 255), TRUE);
-	//左翼
-	DrawTriangle(853, 344, 816, 310, 787, 385, GetColor(255, 255, 255), TRUE);
-	DrawTriangle(853, 344, 816, 310, 814, 380, GetColor(255, 255, 255), TRUE);
-	DrawTriangle(855, 344, 816, 310, 835, 375, GetColor(255, 255, 255), TRUE);
-
+	// 配列の各頂点を利用して三角形を描画する
+	for (int i = 0; i < NUM_VERTICES - 2; i += 3) {
+		if (i < 22) {
+			DrawTriangle(vertices[i].x, vertices[i].y,vertices[i + 1].x, vertices[i + 1].y,vertices[i + 2].x, vertices[i + 2].y,GetColor(255, 255,255), TRUE);
+		}
+		//ひし形の描画
+		else
+		{
+			DrawQuadrangle(vertices[i].x, vertices[i].y,vertices[i + 1].x, vertices[i + 1].y,vertices[i + 2].x, vertices[i + 2].y,vertices[i + 3].x, vertices[i + 3].y,GetColor(255, 255, 255), TRUE);
+			i++;
+		}
+	}
+	DrawFormatString(300, 300, 0xfffff, "%f    %f", vertices[0].x, vertices[0].y);
 }
 
 void EnemyBat::Move()
 {
-
+	// vertices の各要素の座標を local_location にコピーする
+	for (int i = 0; i < NUM_VERTICES; ++i) {
+		local_location.x = vertices[i].x;
+		local_location.y = vertices[i].y;
+	}
+	
 }
 
 void EnemyBat::BatArray()
 {
-	//const int vertex_count = 32;
-	Location vertices[32] = {
-		// 耳
-		{850, 300}, {850, 325}, {862, 312},
-		{880, 300}, {880, 325}, {868, 312},
-		// 頭
-		{865, 316}, {880, 330}, {865, 340}, {850, 330},
-		// 胴体
-		{865, 345}, {880, 368}, {865, 395}, {850, 368},
-		//右翼
-		{877, 344}, {917, 310}, {943, 385},
-		{877, 344}, {917, 310}, {916, 380},
-		{875, 344}, {917, 310}, {895, 375},
-		//左翼
-		{853, 344}, {816, 310}, {787, 385},
-		{853, 344}, {816, 310}, {814, 380},
-		{855, 344}, {816, 310}, {835, 375},
-	};
 }
 
 void EnemyBat::Hit(Location _location, Erea _erea, int _type)
--- TGS_Sitazumi/Object/EnemyBat.h
+++ TGS_Sitazumi/Object/EnemyBat.h
@@ -6,6 +6,8 @@
 #include<sstream>
 #include "Object.h"
 
+#define NUM_VERTICES 32 //頂点の数
+
 enum class BatState {
 	IDLE = 0,
 	LEFT,
@@ -18,8 +20,26 @@ class EnemyBat:
 {
 private:
 	//std::vector<std::vector<int>>vertices;
-	int x_move;
-	Location vertices[32];
+	//Location vertices[32];
+
+	// Location構造体の配列を定義
+	Location vertices[NUM_VERTICES] = {
+		// 耳
+		{850, 300}, {850, 325}, {862, 312},
+		{880, 300}, {880, 325}, {868, 312},
+		//右翼
+		{877, 344}, {917, 310}, {943, 385},
+		{877, 344}, {917, 310}, {916, 380},
+		{875, 344}, {917, 310}, {895, 375},
+		//左翼
+		{853, 344}, {816, 310}, {787, 385},
+		{853, 344}, {816, 310}, {814, 380},
+		{855, 344}, {816, 310}, {835, 375},
+		// 頭
+		{865, 316}, {880, 330}, {865, 340}, {850, 330},
+		// 胴体
+		{865, 345}, {880, 368}, {865, 395}, {850, 368},
+	};
 
 public:
 	EnemyBat();
