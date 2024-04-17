d69f476fd3cf868b559494070e60ad8f5bcb2ab7
れお <120348353+yamaguchi-reoo@users.noreply.github.com>
2024-04-17 15:59:09 +0900

    vector仕様

--- TGS_Sitazumi/Object/EnemyBat.cpp
+++ TGS_Sitazumi/Object/EnemyBat.cpp
@@ -4,6 +4,24 @@
 EnemyBat::EnemyBat()
 {
 	type = ENEMY;
+	vertices = {
+		// 耳
+		{850, 300}, {850, 325}, {862, 312},
+		{880, 300}, {880, 325}, {868, 312},
+		//右翼
+		{877, 344}, {917, 310}, {943, 385},
+		{877, 344}, {917, 310}, {916, 380},
+		{875, 344}, {917, 310}, {895, 375},
+		//左翼
+		{853, 344}, {816, 310}, {787, 385},
+		{853, 344}, {816, 310}, {814, 380},
+		{855, 344}, {816, 310}, {835, 375},
+		// 頭
+		{865, 316}, {880, 330}, {865, 340}, {850, 330},
+		// 胴体
+		{865, 345}, {880, 368}, {865, 395}, {850, 368},
+	};
+
 }
 
 EnemyBat::~EnemyBat()
@@ -12,19 +30,16 @@ EnemyBat::~EnemyBat()
 
 void EnemyBat::Update()
 {
-	for (int i = 0; i < NUM_VERTICES; i++) {
-		local_location.x = vertices[i].x;
-		local_location.y = vertices[i].y;
-	}
-	Move();
-	BatArray();
+	/*Move();
+	BatArray();*/
 }
 
 void EnemyBat::Draw() const
 {
-	// 配列の各頂点を利用して三角形を描画する
-	for (int i = 0; i < NUM_VERTICES; i += 3) {
+	//配列の各頂点を利用して三角形を描画する
+	for (int i = 0; i < vertices.size(); i += 3) {
 		if (i < 22) {
+			//DrawTriangle(vertices[i].x, vertices[i].y,vertices[i + 1].x, vertices[i + 1].y,vertices[i + 2].x, vertices[i + 2].y,GetColor(255, 255,255), TRUE);
 			DrawTriangle(vertices[i].x, vertices[i].y,vertices[i + 1].x, vertices[i + 1].y,vertices[i + 2].x, vertices[i + 2].y,GetColor(255, 255,255), TRUE);
 		}
 		//ひし形の描画
@@ -34,11 +49,13 @@ void EnemyBat::Draw() const
 			i++;
 		}
 	}
-	DrawFormatString(300, 300, 0xfffff, "%f    %f", vertices[0].x, vertices[0].y);
+
+	//配列の各頂点を利用して三角形を描画する
+	DrawFormatString(300, 300, 0xfffff, "%f    %f", local_location.x, local_location.y);
 }
 
 void EnemyBat::Move()
-{	
+{
 }
 
 void EnemyBat::BatArray()
