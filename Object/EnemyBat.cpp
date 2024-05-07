e1fa06658ec6a6a9420a96b6ea20ee5942dc3f7e
kobayasihinata <120348245+kobayasihinata@users.noreply.github.com>
2024-05-07 15:03:37 +0900

    コンフリクト解消の後処理２

--- Object/EnemyBat.cpp
+++ Object/EnemyBat.cpp
@@ -176,8 +176,7 @@ void EnemyBat::Move()
 void EnemyBat::Hit(Object* _object)
 {
 	//ブロックと当たった時の処理
-	if (_type == BLOCK || _type == ENEMY)
-	if (_object->GetObjectType() == BLOCK)
+	if (_object->GetObjectType() == BLOCK || _object->GetObjectType() == ENEMY)
 	{
 		Location tmpl = location;
 		Erea tmpe = erea;
@@ -319,31 +318,31 @@ void EnemyBat::Hit(Object* _object)
 
 	//赤コウモリ
 	//触れたブロックが緑＆＆自分の色が赤だったら触れた緑ブロックを燃やす
-	if (_type == WOOD && this->color == RED) {
+	if (_object->GetObjectType() == WOOD && this->color == RED) {
 
 	}
 	//水の中に突っ込むと即死　雨粒は即死だが死ぬ際の動きに変化あり
-	if (_type == WATER && this->color == RED) {
+	if (_object->GetObjectType() == WATER && this->color == RED) {
 
 	}
 
 	//青コウモリ
 	//触れたブロックが赤＆＆自分の色が青だったら触れた赤ブロックを消す
-	if (_type == FIRE && this->color == BLUE) {
+	if (_object->GetObjectType() == FIRE && this->color == BLUE) {
 
 	}
 	//コウモリの色が吸い取られて死ぬ
-	if (_type == WOOD && this->color == BLUE) {
+	if (_object->GetObjectType() == WOOD && this->color == BLUE) {
 
 	}
 
 	//緑コウモリ
 	//触れたブロックが青＆＆自分の色が緑だったら、雨粒は吸い取り　水場などに当たると反射する
-	if (_type == WATER && this->color == GREEN) {
+	if (_object->GetObjectType() == WATER && this->color == GREEN) {
 
 	}
 	//当たったら即死
-	if (_type == FIRE && this->color == GREEN) {
+	if (_object->GetObjectType() == FIRE && this->color == GREEN) {
 
 	}
 }
