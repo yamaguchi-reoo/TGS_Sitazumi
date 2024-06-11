4da85c89efbf380d7d4d5b1a996f7fa21b774db4
れお <120348353+yamaguchi-reoo@users.noreply.github.com>
2024-06-11 14:55:38 +0900

    まだまだ調整中でございます

--- Object/Boss.cpp
+++ Object/Boss.cpp
@@ -39,8 +39,8 @@ Boss::~Boss()
 
 void Boss::Initialize(Location _location, Erea _erea, int _color_data, int _object_pos)
 {
-	location = { SCREEN_WIDTH - 200, SCREEN_HEIGHT - 300};//x座標 ,y座標 
-	//location = { SCREEN_WIDTH / 2, SCREEN_HEIGHT - 300 };//x座標 ,y座標 
+	//location = { SCREEN_WIDTH - 200, SCREEN_HEIGHT - 300};//x座標 ,y座標 
+	location = { SCREEN_WIDTH / 2, SCREEN_HEIGHT - 300 };//x座標 ,y座標 
 	erea = { _erea };	   //高さ、幅	
 	color = _color_data;
 
@@ -72,7 +72,7 @@ void Boss::Update(GameMain* _g)
 	vector = { 1.0f ,1.0f };
 
 	SetPosition();  // 更新時に座標を保存
-	//SetWingPositions();
+	SetWingPositions();
 	UpdateWingPositions();
 	Location player_pos = _g->GetPlayerLocation();
 
@@ -85,7 +85,7 @@ void Boss::Update(GameMain* _g)
 			Move(_g);
 			break;
 		case BossState::ATTACK:
-			BossAtack(_g);
+			//BossAtack(_g);
 			break;
 		case BossState::DEATH:
 			_g->DeleteObject(object_pos);
@@ -205,11 +205,11 @@ void Boss::Draw() const
 	//DrawFormatString(1100, 60, color, "%d", boss_state);
 	//DrawFormatString(1100, 60, color, "%d", cnt);
 	//DrawFormatString(1000, 20, color, "%f   %f", KeyInput::GetMouseCursor());
-	SetFontSize(24);
-	DrawFormatString(800, 30, color, "wingNumber : %d", cunt);
-	//DrawFormatString(800, 50, color, "wing : %d", c);
-	DrawFormatString(800, 50, color, "wingmirror : %f", wing_mirror[0].x);
-	DrawFormatString(800, 80, color, "wingmirror : %f", wing_mirror[0].y);
+	//SetFontSize(24);
+	//DrawFormatString(800, 30, color, "wingNumber : %d", cunt);
+	////DrawFormatString(800, 50, color, "wing : %d", c);
+	//DrawFormatString(800, 50, color, "wingmirror : %f", wing_mirror[0].x);
+	//DrawFormatString(800, 80, color, "wingmirror : %f", wing_mirror[0].y);
 
 }
 
@@ -450,6 +450,13 @@ void Boss::DrawWings() const
 			local_location.x + wing[i + 2].x + 10, local_location.y + wing[i + 2].y + 20,
 			local_location.x + wing[i + 3].x + 0, local_location.y + wing[i + 3].y + 30, 0x000000, TRUE);
 	}
+
+	for (int i = 0; i < wing.size(); i += 4) {
+		DrawQuadrangleAA(local_location.x + wing_mirror[i].x, local_location.y + wing_mirror[i].y,
+			local_location.x + wing_mirror[i + 1].x + 20, local_location.y + wing_mirror[i + 1].y + 10,
+			local_location.x + wing_mirror[i + 2].x + 10, local_location.y + wing_mirror[i + 2].y + 20,
+			local_location.x + wing_mirror[i + 3].x + 0, local_location.y + wing_mirror[i + 3].y + 30, 0x000000, TRUE);
+	}
 }
 
 void Boss::UpdateWingPositions()
@@ -490,7 +497,7 @@ void Boss::SetWingPositions()
 	for (size_t i = 0; i < wing.size(); ++i)
 	{
 		// wing[i] の X 座標を反転して wing_mirror[i] の X 座標にセット
-		wing_mirror[i].x = center.x - (wing[i].x - center.x); // X座標を反転
+		wing_mirror[i].x = -wing[i].x; // X座標を反転
 
 		// wing[i] の Y 座標をそのまま wing_mirror[i] の Y 座標にセット
 		wing_mirror[i].y = wing[i].y;
